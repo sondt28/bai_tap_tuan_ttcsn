55acf0f4ae3e0ece3f15f3501d5c3dfcea8d7b80
Son <son286202@gmail.com>
2023-03-25 21:32:24 +0700

    bai 1

---
+++ BaiTapTuan/Controllers/HelloworldController1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapTuan.Controllers
+{
+    public class HelloworldController1 : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
