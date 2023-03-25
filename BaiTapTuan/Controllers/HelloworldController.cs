f5c10f8fc731428de159ef42f123f7871bc343e5
Son <son286202@gmail.com>
2023-03-25 21:48:46 +0700

    add view

---
+++ BaiTapTuan/Controllers/HelloworldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace BaiTapTuan.Controllers
+{
+    public class HelloworldController : Controller
+    {
+        public IActionResult Index(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
